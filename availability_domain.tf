cecaf6d84ad774207ef8db5e8074cf91eae28b82
Bilegt <bilegt.bat.ochir@oracle.com>
2021-04-05 13:55:38 +0300

    Update availability_domain.tf

--- availability_domain.tf
+++ availability_domain.tf
@@ -6,3 +6,8 @@ data "oci_identity_fault_domains" "fault_domains" {
     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
     compartment_id = var.compartment_ocid
 }
+
+resource "random_string" "deploy_id" {
+  length  = 4
+  special = false
+}
