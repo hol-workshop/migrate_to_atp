212eb0d41c49e39ada4a0f2ceb7dd407ce322976
Bilegt <bilegt.bat.ochir@oracle.com>
2021-05-17 11:07:21 +0300

    Update outputs.tf

--- ogg_pgsql/outputs.tf
+++ ogg_pgsql/outputs.tf
@@ -1,11 +1,11 @@
 // Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
 
 output "image_id" {
-  value = "${oci_core_instance.vm1.source_details.0.source_id}"
+  value = oci_core_instance.vm1.source_details.0.source_id
 }
 
 output "instance_id" {
-  value = "${oci_core_instance.vm1.id}"
+  value = oci_core_instance.vm1.id
 }
 
 output "OGG_PGSQL_Public_ip" {
